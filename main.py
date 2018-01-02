e5f878f3f7a5f4156b714cc74f642d86843d6061
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2018-01-02 00:00:12 -0500

    0.0.2 changed mp file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 #create main to listen to music
 def main():
     pygame.mixer.init(frequency=44100)
-    pygame.mixer.music.load("9784864715225.mp3")
+    pygame.mixer.music.load("ted.mp3")
     pygame.mixer.music.play(1)
     time.sleep(100)
     pygame.mixer.music.stop()
